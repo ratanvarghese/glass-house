dad280f7752c7b23c1e7f05764308e5d18e3afdc
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2019-03-05 22:25:39 -0500

    Block movement with walls

--- main.lua
+++ main.lua
@@ -1,21 +1,57 @@
 local termfx = require("termfx")
 
+local MAX_X, MAX_Y = 70, 20
+
+function getIdx(x, y)
+	return (y*MAX_X) + x
+end
+
 local symbols = {
-	floor = " ",
+	floor = ".",
 	wall = "#",
 	player = "@",
 	stair = ">"
 }
 
-local denizens = {
-	{
-		symbol = symbols.player,
-		x = 10,
-		y = 10
-	}
-}
+local denizens = {}
+local terrain = {}
 
+function move(old_x, old_y, new_x, new_y)
+	local new_id = getIdx(new_x, new_y)
 
+	if terrain[new_id] and terrain[new_id].symbol == symbols.wall then
+		return false
+	elseif denizens[new_id] then
+		return false
+	end
+
+	local old_id = getIdx(old_x, old_y)
+	local d = denizens[old_id]
+	d.x = new_x
+	d.y = new_y
+	denizens[new_id] = d
+	denizens[old_id] = nil
+	return true
+end
+
+for y=1,MAX_Y do
+	for x=1,MAX_X do
+		local s
+		if x == 1 or x == MAX_X or y == 1 or y == MAX_Y then
+			s = symbols.wall
+		else
+			s = symbols.floor
+		end
+		if terrain[getIdx(x, y)] then
+			s = "!"
+		end
+		terrain[getIdx(x, y)] = {symbol = s, x = x, y = y}
+	end
+end
+
+local init_x, init_y = 10, 10
+local player_id = getIdx(init_x, init_y)
+denizens[player_id] = {symbol = symbols.player, x = init_x, y = init_y}
 
 termfx.init()
 
@@ -24,20 +60,33 @@ local ok, err = pcall(function()
 
 while keepGoing do
 	termfx.clear()
-	termfx.printat(denizens[1].x, denizens[1].y, denizens[1].symbol)
+
+	for _,tile in pairs(terrain) do
+		termfx.printat(tile.x, tile.y, tile.symbol)
+	end
+	for _,denizen in pairs(denizens) do
+		termfx.printat(denizen.x, denizen.y, denizen.symbol)
+	end
+
 	termfx.present()
 
 	local evt = termfx.pollevent()
+	local dy, dx = 0, 0
 	if evt.char == "q" then
 		keepGoing = false
 	elseif evt.char == "w" then
-		denizens[1].y = denizens[1].y - 1
+		dy = -1
 	elseif evt.char == "s" then
-		denizens[1].y = denizens[1].y + 1
+		dy = 1
 	elseif evt.char == "a" then
-		denizens[1].x = denizens[1].x - 1
+		dx = -1
 	elseif evt.char == "d" then
-		denizens[1].x = denizens[1].x + 1
+		dx = 1
+	end
+
+	local p = denizens[player_id]
+	if move(p.x, p.y, p.x + dx, p.y + dy) then
+		player_id = getIdx(p.x, p.y)
 	end
 end
 
