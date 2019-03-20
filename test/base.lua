cb6578f2733b2375cf82c8beefb13bec60bd3654
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2019-03-20 00:02:11 -0400

    Add test/base.lua

---
+++ test/base.lua
@@ -0,0 +1,67 @@
+local base = require("src.base")
+
+local unique_idx_set = {}
+property "base.getIdx: unique idx" {
+	generators = { int(1, base.MAX_X), int(1, base.MAX_Y) },
+	check = function(x, y)
+		local i = base.getIdx(x, y)
+		local alreadyFound = unique_idx_set[i]
+		unique_idx_set[i] = {x=x, y=y}
+		if alreadyFound then
+			return alreadyFound.x == x and alreadyFound.y == y
+		else
+			return true
+		end
+	end
+}
+
+property "base.getIdx: error on string" {
+	generators = { str(), int(1, base.MAX_X) },
+	check = function(s, i)
+		local ok_1 = pcall(function() base.getIdx(s,i) end)
+		local ok_2 = pcall(function() base.getIdx(i,s) end)
+		return (not ok_1) and (not ok_2)
+	end
+}
+property "base.getIdx: error on table" {
+	generators = { tbl(), int(1, base.MAX_X) },
+	check = function(s, i)
+		local ok_1 = pcall(function() base.getIdx(s,i) end)
+		local ok_2 = pcall(function() base.getIdx(i,s) end)
+		return (not ok_1) and (not ok_2)
+	end
+}
+property "base.getIdx: error on bool" {
+	generators = { bool(), int(1, base.MAX_X) },
+	check = function(s, i)
+		local ok_1 = pcall(function() base.getIdx(s,i) end)
+		local ok_2 = pcall(function() base.getIdx(i,s) end)
+		return (not ok_1) and (not ok_2)
+	end
+}
+
+local function smallGrid(x, y, v1, v2, v3, v4)
+	return {
+		[base.getIdx(x,y+1)] = v1,
+		[base.getIdx(x,y-1)] = v2,
+		[base.getIdx(x+1,y)] = v3,
+		[base.getIdx(x-1,y)] = v4
+	}
+end
+property "base.adjacent_min: pick minimum value" {
+	generators = { int(1, base.MAX_X), int(1, base.MAX_Y), int(), int(), int(), int() },
+	check = function(x, y, v1, v2, v3, v4)
+		local grid = smallGrid(x, y, v1, v2, v3, v4)
+		local res_v = base.adjacent_min(grid, x, y)
+		return res_v == math.min(v1, v2, v3, v4)
+	end
+}
+property "base.adjacent_min: pick minimum coordinates" {
+	generators = { int(1, base.MAX_X), int(1, base.MAX_Y), int(), int(), int(), int() },
+	check = function(x, y, v1, v2, v3, v4)
+		local grid = smallGrid(x, y, v1, v2, v3, v4)
+		local _, res_x, res_y = base.adjacent_min(grid, x, y)
+		return grid[base.getIdx(res_x, res_y)] == math.min(v1, v2, v3, v4)
+	end
+}
+
