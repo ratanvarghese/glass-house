0ec1d99453723e67307c89e44ae3e920bef61ae7
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2019-05-28 20:35:57 -0400

    Added test/lqc/bestiary

---
+++ test/lqc/bestiary.lua
@@ -0,0 +1,62 @@
+local grid = require("src.grid")
+local base = require("src.base")
+local tool = require("src.tool")
+local bestiary = require("src.bestiary")
+
+bestiary.set.centurion = {
+	kind = 100,
+	hp = 200,
+	light_radius = 300,
+	inventory = {
+		"lantern"
+	}
+}
+
+bestiary.set.gladiator = {
+	kind = 101,
+	hp = 400
+}
+
+local test_names = {"centurion", "gladiator"}
+
+property "bestiary.make: x, y match input" {
+	generators = { int(1, grid.MAX_X), int(1, grid.MAX_Y), int(1, #test_names) },
+	check = function(x, y, name_i)
+		local name = test_names[name_i] 
+		local res = bestiary.make(name, x, y)
+		return res.x == x and res.y == y
+	end
+}
+
+property "bestiary.make: simple properties match" {
+	generators = { int(1, grid.MAX_X), int(1, grid.MAX_Y), int(1, #test_names) },
+	check = function(x, y, name_i)
+		local name = test_names[name_i]
+		local res = bestiary.make(name, x, y)
+		for k,v in pairs(bestiary.set[name]) do
+			if k ~= "inventory" and res[k] ~= v then
+				return false
+			end
+		end
+		return true
+	end
+}
+
+property "bestiary.make: inventory are tool objects" {
+	generators = { int(1, grid.MAX_X), int(1, grid.MAX_Y), int(1, #test_names) },
+	check = function(x, y, name_i)
+		local name = test_names[name_i]
+		local res = bestiary.make(name, x, y)
+		if bestiary.set[name].inventory then
+			for i,v in ipairs(bestiary.set[name].inventory) do
+				local obj = tool.make(v)
+				if not base.equals(res.inventory[i], obj) then
+					return false
+				end
+			end
+			return true
+		else
+			return base.is_empty(res.inventory)
+		end
+	end
+}
