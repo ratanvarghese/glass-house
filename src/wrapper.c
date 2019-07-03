6602621ba10347d6cc7f925e97522e51527e6748
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2019-07-03 17:03:36 -0400

    Minor changes to wrapper.c

--- src/wrapper.c
+++ src/wrapper.c
@@ -4,23 +4,21 @@
 #include <lua.h>
 #include <lualib.h>
 
-#include "body.h"
+#include "body.h" /*Generated during build*/
 
 int main(int argc, char** argv) {
-	lua_State *L = lua_open();
+	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
 
-	lua_createtable(L, (argc>0 ? argc-1 : 0), 1); /*zero index not an array element in Lua?*/
+	lua_createtable(L, argc, 1);
 	for(int i = 0; i < argc; i++) {
 		lua_pushstring(L, argv[i]);
 		lua_rawseti(L, -2, i);
 	}
 	lua_setglobal(L, "arg");
 
-	int error = luaL_loadbuffer(L, luaJIT_BC_body, sizeof(luaJIT_BC_body), "glass-house") || lua_pcall(L, 0, 0, 0);
-	if(error) {
-		fprintf(stderr, "%s", lua_tostring(L, -1));
-	}
+	luaL_loadbuffer(L, luaJIT_BC_body, sizeof(luaJIT_BC_body), "glass-house");
+	lua_call(L, 0, 0);
 	lua_close(L);
 	return 0;
 }
