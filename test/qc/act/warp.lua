06acb2ea776b45caf516b5ca917aea010074972a
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2019-08-23 16:43:28 -0400

    Minor test change

--- test/qc/act/warp.lua
+++ test/qc/act/warp.lua
@@ -180,7 +180,7 @@ property "act[enum.power.warp].pursue: utility" {
 	end
 }
 
-property "act[enum.power.mundane].pursue: attempt if obviously possible" {
+property "act[enum.power.warp].pursue: attempt if obviously possible" {
 	generators = { int(), int(3, grid.MAX_X-2), int(3, grid.MAX_Y-2), int(2, 10) },
 	check = attempt_if_possible(act[enum.power.warp].pursue)
 }
